f82bfda80c7733836cdadadd8bc05d6460b3ef1b
camp5456 <tcamp@ou.edu>
2014-04-14 11:25:37 -0500

    Removing top navigation bar
    Removing the black top navigation bar

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -137,41 +137,6 @@
       
 <div class="container">
  
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?> navbar-inverse">
-  <div class="container">
-    <div class="navbar-header">
-      
-
-     
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <!-- <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span> -->
-        <p class="small white">MENU</p>
-      </button>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
-  </div>
-</header>
-
 <!-- body tag for stick footer -->
 <div id="body">
 
